2cb489a4bc4132efc92abda3ee0cadef491ec6cf
Kirti Bansal <kirti.bansal@oracle.com>
2026-07-08 21:00:52 +0530

    Initial JavaFX project setup

---
+++ alm-system/src/main/java/com/alm/application/Main.java
@@ -0,0 +1,24 @@
+package com.alm.application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("ALM System");
+        StackPane root = new StackPane(label);
+
+        Scene scene = new Scene(root, 800, 500);
+        stage.setTitle("ALM System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
